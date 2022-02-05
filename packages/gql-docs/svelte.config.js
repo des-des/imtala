5aade4de32fc5fc18abc3223b86807ae137f2258
Eoin McCarthy <em1081@my.bristol.ac.uk>
2022-02-05 11:29:32 +0000

    build fixes

--- packages/gql-docs/svelte.config.js
+++ packages/gql-docs/svelte.config.js
@@ -1,12 +1,30 @@
 // import adapter from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-auto';
+import { mdsvex } from 'mdsvex';
+
+const mdsvexConfig = {
+	extensions: ['.md'],
+
+	smartypants: {
+		dashes: 'oldschool'
+	},
+
+	remarkPlugins: [],
+	rehypePlugins: []
+};
+
+
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	extensions: ['.svelte', ...mdsvexConfig.extensions],
+
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
-	preprocess: preprocess(),
+	// preprocess: preprocess(),
+	preprocess: [preprocess(), mdsvex(mdsvexConfig)],
+
 
 	kit: {
 		adapter: adapter({
@@ -14,8 +32,55 @@ const config = {
 		}),
 
 		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte'
+		target: '#svelte',
+
+		vite: {
+			server: {
+				fs: {
+					allow: ['README.md']
+				}
+			}
+		}
 	}
 };
 
 export default config;
+
+
+
+// import adapter from '@sveltejs/adapter-static';
+// // import adapter from '@sveltejs/adapter-auto';
+// import preprocess from 'svelte-preprocess';
+// // import svelte from 'rollup-plugin-svelte'
+// import { mdsvex } from 'mdsvex';
+
+// const mdsvexConfig = {
+// 	extensions: ['.md'],
+
+// 	smartypants: {
+// 		dashes: 'oldschool'
+// 	},
+
+// 	remarkPlugins: [],
+// 	rehypePlugins: []
+// };
+
+// /** @type {import('@sveltejs/kit').Config} */
+// const config = {
+// 	extensions: ['.svelte', ...mdsvexConfig.extensions],
+// 	// Consult https://github.com/sveltejs/svelte-preprocess
+// 	// for more information about preprocessors
+// 	preprocess: [preprocess(), mdsvex(mdsvexConfig)],
+// 	kit: {
+// 		paths: {
+// 			assets: 'https://des-des.github.io/imtala',
+// 			base: '/imtala'
+// 		},
+// 		adapter: adapter(),
+
+// 		// hydrate the <div id="svelte"> element in src/app.html
+// 		target: '#svelte'
+// 	}
+// };
+
+// export default config;
