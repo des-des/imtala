98a5e4dab1515ef1a07fea5bb10981dd93ed05c0
Eoin McCarthy <em1081@my.bristol.ac.uk>
2022-04-06 19:01:03 +0100

    versions and condif for client publish

--- packages/client/src/hooks.ts
+++ packages/client/src/hooks.ts
@@ -1,6 +1,5 @@
 /** @type {import('@sveltejs/kit').Handle} */
 export async function handle({ event, resolve }) {
-    console.log(event.url.pathname)
     const response = await resolve(event, {
       ssr: !event.url.pathname.startsWith('/connection') && !(event.url.pathname === '/'),
     });
